4177d6f9a64eb0bf55cd84feb3f8399983656204
namewyf <115757010+namewyf@users.noreply.github.com>
2022-10-15 20:37:25 +0800

    Add files via upload

---
+++ question10.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	printf("%d–° ±",a/60);
+	printf("%d∑÷÷”",a%60);
+	 
+}
---
+++ question2.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	
+	printf("   A\n  AAA\n AAAAA\nAAAAAAA\n AAAAA\n  AAA\n   A");
+ } 
